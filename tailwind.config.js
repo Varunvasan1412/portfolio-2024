2c68d6583ea19e359eec93348a6353a399af9922
Your Name <your.email@example.com>
2024-09-14 21:34:23 +0530

    feat: created a navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: [
+    // or if using src directory:
+    "./src/**/*.{js,ts,jsx, tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: "#1b1b1b",
+        light: "#f5f5f5",
+        primary: "#B63E96", // 240,86,199
+        primaryDark: "#58E6D9", // 80,230,217
+      },
+    },
   },
   plugins: [],
 };
