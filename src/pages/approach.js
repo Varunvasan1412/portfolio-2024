6f8b816b7bf5f99be2e8e0a4e148bb7da6eff978
Your Name <your.email@example.com>
2024-09-18 20:03:07 +0530

    Fix: responsive bugs

--- src/pages/approach.js
+++ src/pages/approach.js
@@ -1,10 +1,8 @@
 "use client";
 import React from "react";
-
+import Head from "next/head";
 import { AnimatePresence, motion } from "framer-motion";
 import { CanvasRevealEffect } from "@/components/CanvasRevealEffect";
-import Head from "next/head";
-import Layout from "@/components/Layout";
 import AnimatedText from "@/components/AnimatedText";
 
 const approach = () => {
@@ -14,46 +12,45 @@ const approach = () => {
         <title>Varun | My Approach</title>
         <meta name="description" content="any description" />
       </Head>
-      <Layout>
-        <AnimatedText text="Strategize for success." className="mb-16" />
-        <div className="my-20 flex flex-col lg:flex-row items-center justify-center border bg-light/50  w-full gap-4">
-          <Card
-            title="Planning & Strategy"
-            icon={<AceternityIcon />}
-            description="I'll collaborate with you to map out your website's goals, target audience, and key functionalities. I’ll help you with aspects like site structure, navigation, and content requirements."
-          >
-            <CanvasRevealEffect
-              animationSpeed={3}
-              containerClassName="bg-emerald-900"
-            />
-          </Card>
-          <Card
-            title="Development & Progress Update"
-            icon={<AceternityIcon />}
-            description="Once I agree on the plan, I cue up my lofi playlist and dive into coding. From initial sketches to polished code, I keep you updated every step of the way."
-          >
-            <CanvasRevealEffect
-              animationSpeed={3}
-              containerClassName="bg-yellow-900"
-              colors={[[255, 0, 0]]}
-              dotSize={8}
-            />
-            {/* Radial gradient for the cute fade */}
-            <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" />
-          </Card>
-          <Card
-            title="Development & Launch"
-            icon={<AceternityIcon />}
-            description="This is where the magic happens! Based on the approved design, I'll translate everything into functional code, building your website from the ground up."
-          >
-            <CanvasRevealEffect
-              animationSpeed={3}
-              containerClassName="bg-sky-600"
-              colors={[[125, 211, 252]]}
-            />
-          </Card>
-        </div>
-      </Layout>
+      <AnimatedText text="Strategize for success." className="mb-16 pt-16" />
+      <div className="my-20 flex flex-col lg:flex-row items-center justify-center gap-4">
+        <Card
+          title="Planning & Strategy"
+          icon={<AceternityIcon />}
+          description="I'll collaborate to map out your website's goals, target audience, and key functionalities. I'll discuss things like site structure, navigation and content requirements."
+        >
+          <CanvasRevealEffect
+            animationSpeed={5.1}
+            containerClassName="bg-emerald-900"
+          />
+        </Card>
+        <Card
+          title="Development & Progress Update"
+          icon={<AceternityIcon />}
+          description="Once we agree on the plan, I cue my lofi playlist and dive into coding. From initial sketches to polished code, I keep you updated every step of the way."
+        >
+          <CanvasRevealEffect
+            animationSpeed={3}
+            containerClassName="bg-black"
+            colors={[
+              [236, 72, 153],
+              [232, 121, 249],
+            ]}
+            dotSize={2}
+          />
+        </Card>
+        <Card
+          title="Development & Launch"
+          icon={<AceternityIcon />}
+          description="This is where the magic happens! Based on the approved design, I'll translate everything into functional code, building your website from the ground up."
+        >
+          <CanvasRevealEffect
+            animationSpeed={3}
+            containerClassName="bg-sky-600"
+            colors={[[125, 211, 252]]}
+          />
+        </Card>
+      </div>
     </>
   );
 };
@@ -64,7 +61,7 @@ const Card = ({ title, icon, children, description }) => {
     <div
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
-      className="group/canvas-card flex items-center justify-center dark:border-white/[0.2]  max-w-sm w-full mx-auto p-4 relative lg:h-[35rem] rounded-3xl"
+      className="border border-black/[0.2] group/canvas-card flex items-center justify-center dark:border-white/[0.2]  max-w-sm w-full mx-auto p-4 relative lg:h-[35rem] rounded-3xl"
     >
       <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
       <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
@@ -85,10 +82,10 @@ const Card = ({ title, icon, children, description }) => {
         <div className="text-center group-hover/canvas-card:-translate-y-4 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] group-hover/canvas-card:opacity-0 transition duration-200 w-full  mx-auto flex items-center justify-center">
           {icon}
         </div>
-        <h2 className="dark:text-white text-3xl text-center opacity-0 group-hover/canvas-card:opacity-100 relative z-10 text-black mt-4  font-bold group-hover/canvas-card:text-white group-hover/canvas-card:-translate-y-2 transition duration-200">
+        <h2 className="dark:text-white opacity-0 group-hover/canvas-card:opacity-100 relative z-10 text-black mt-4  font-bold group-hover/canvas-card:text-white group-hover/canvas-card:-translate-y-2 transition duration-200 text-3xl text-center">
           {title}
         </h2>
-        <h2 className="dark:text-white text-sm text-center opacity-0 group-hover/canvas-card:opacity-100 relative z-10 text-black mt-4  font-bold group-hover/canvas-card:text-white group-hover/canvas-card:-translate-y-2 transition duration-200">
+        <h2 className="dark:text-white text-sm opacity-0 group-hover/canvas-card:opacity-100 relative z-10 text-black mt-4  font-bold group-hover/canvas-card:text-white group-hover/canvas-card:-translate-y-2 transition duration-200 text-center style = {{color: '#e4ecff">
           {description}
         </h2>
       </div>
@@ -98,12 +95,14 @@ const Card = ({ title, icon, children, description }) => {
 
 const AceternityIcon = () => {
   return (
-    <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-800 focus:ring-offset-8 focus:ring-offset-slate-50">
-      <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-      <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 font-bold px-5 py-2 text-white backdrop-blur-3xl text-2xl">
-        Lumos !
-      </span>
-    </button>
+    <div>
+      <button className="relative inline-flex lg:h-14 overflow-hidden rounded-full p-[3px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-[8px] focus:ring-offset-slate-50">
+        <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+        <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-5 py-3  text-white backdrop-blur-3xl text-sm lg:text-lg font-bold">
+          Hover Me !
+        </span>
+      </button>
+    </div>
   );
 };
 
@@ -124,39 +123,3 @@ export const Icon = ({ className, ...rest }) => {
 };
 
 export default approach;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
