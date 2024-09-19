b2adb3cc947965d27ef51622087edefc3dbdd0ed
Your Name <your.email@example.com>
2024-09-19 23:37:38 +0530

    fix: bugs

--- src/pages/about.js
+++ src/pages/about.js
@@ -53,21 +53,21 @@ const about = () => {
               </h2>
 
               <p className="font-medium text-[16px]">
-                Hi, I'm Varun, a web developer and UI/UX designer with a passion
-                for creating beautiful, functional, and user-centered digital
-                experiences. With 4 years of experience in the field. I am
-                always looking for new and innovative ways to bring my clients'
-                visions to life.
+                Hi, I am Varun, a web developer and UI/UX designer with a
+                passion for creating beautiful, functional, and user-centered
+                digital experiences. With 4 years of experience in the field. I
+                am always looking for new and innovative ways to bring my
+                clients visions to life.
               </p>
 
               <p className="font-medium text-[16px] my-4">
                 I believe that design is about more than just making things look
-                pretty – it's about solving problems and creating intuitive,
+                pretty it's about solving problems and creating intuitive,
                 enjoyable experiences for users.
               </p>
 
               <p className="font-medium text-[16px]">
-                Whether I'm working on a website, mobile app, or other digital
+                Whether I am working on a website, mobile app, or other digital
                 product, I bring my commitment to design excellence and
                 user-centered thinking to every project I work on. I look
                 forward to the opportunity to bring my skills and passion to
